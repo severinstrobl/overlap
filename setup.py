3eb02f00bbed6de2160cfd9f8c6aea639e5bb0bc
Severin Strobl <git@severin-strobl.de>
2021-01-02 14:17:29 +0100

    Added integration for setuptools/scikit-build
    Using scikit-build allows for a nicer integration of CMake than
    distutils.core.Extension and supports pyproject.toml according to
    PEP 518.

---
+++ setup.py
@@ -0,0 +1,36 @@
+from skbuild import setup, exceptions, cmaker
+from packaging.version import LegacyVersion
+
+# Add CMake as a build requirement if no or only an outdated version is
+# available.
+setup_requires = []
+try:
+    if LegacyVersion(cmaker.get_cmake_version()) < LegacyVersion('3.10'):
+        setup_requires.append('cmake')
+except exceptions.SKBuildError:
+    setup_requires.append('cmake')
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='overlap',
+    version='0.0.5',
+    author='Severin Strobl',
+    author_email='git@severin-strobl.de',
+    description='Exact calculation of the overlap volume and area of spheres '
+                'and mesh elements',
+    url='https://github.com/severinstrobl/overlap',
+    license='GPLv3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['overlap'],
+    package_dir={'overlap': 'python'},
+    setup_requires=setup_requires,
+    cmake_install_dir='python',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Topic :: Scientific/Engineering'
+    ]
+)
